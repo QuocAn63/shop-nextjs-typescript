4937d52b1e146dc22461654cdf430110a723410d
CaoQuocAn <caoan632002@gmail.com>
2023-02-18 17:14:34 +0700

    create footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,80 @@
+import { Col, Row } from "antd";
+
 const Footer = () => {
-  return <div></div>;
+  return <div className="bg-black w-full min-h-[220px] flex justify-center">
+    <div className="max-w-screen-xl w-full pt-[35px] pb-[55px]">
+      <Row className="text-white" gutter={30}>
+        <Col span={6}>
+          <h3 className="uppercase text-base pb-5">Thông tin liên hệ</h3>
+          <div>
+            <p className="pb-3">
+              <strong>Địa chỉ:</strong>
+              <strong>123 Ho Chi Minh city, Vietnam</strong>
+            </p>
+            <p className="pb-3">
+              <strong>Hotline:</strong>
+              <strong>+84 90341123</strong>
+            </p>
+            <p className="pb-3">
+              <strong>Email:</strong>
+              <strong>sneakerstore@gmail.com</strong>
+            </p>
+          </div>
+        </Col>
+                <Col span={6}>
+          <h3 className="uppercase text-base pb-5">Thông tin liên hệ</h3>
+          <div>
+            <p className="pb-3">
+              <strong>Địa chỉ:</strong>
+              <strong>123 Ho Chi Minh city, Vietnam</strong>
+            </p>
+            <p className="pb-3">
+              <strong>Hotline:</strong>
+              <strong>+84 90341123</strong>
+            </p>
+            <p className="pb-3">
+              <strong>Email:</strong>
+              <strong>sneakerstore@gmail.com</strong>
+            </p>
+          </div>
+        </Col>
+                <Col span={6}>
+          <h3 className="uppercase text-base pb-5">Thông tin liên hệ</h3>
+          <div>
+            <p className="pb-3">
+              <strong>Địa chỉ:</strong>
+              <strong>123 Ho Chi Minh city, Vietnam</strong>
+            </p>
+            <p className="pb-3">
+              <strong>Hotline:</strong>
+              <strong>+84 90341123</strong>
+            </p>
+            <p className="pb-3">
+              <strong>Email:</strong>
+              <strong>sneakerstore@gmail.com</strong>
+            </p>
+          </div>
+        </Col>
+                <Col span={6}>
+          <h3 className="uppercase text-base pb-5">Thông tin liên hệ</h3>
+          <div>
+            <p className="pb-3">
+              <strong>Địa chỉ:</strong>
+              <strong>123 Ho Chi Minh city, Vietnam</strong>
+            </p>
+            <p className="pb-3">
+              <strong>Hotline:</strong>
+              <strong>+84 90341123</strong>
+            </p>
+            <p className="pb-3">
+              <strong>Email:</strong>
+              <strong>sneakerstore@gmail.com</strong>
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  </div>;
 };
 
 export default Footer;
