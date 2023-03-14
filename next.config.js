0c670fbeab07b8b429eec6d37c015d78e9bfac05
CaoQuocAn <caoan632002@gmail.com>
2023-03-14 21:31:20 +0700

    update: db connector

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    mongo_uri:
+      "mongodb+srv://caoan632002:0903413280An@cluster0.7wk1tu3.mongodb.net/test",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
