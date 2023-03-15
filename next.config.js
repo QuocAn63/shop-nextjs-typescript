bcb5a9e4467ea3c7b0e52aabe88ea4bee113f3b3
CaoQuocAn <caoan632002@gmail.com>
2023-03-15 20:32:29 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,15 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-<<<<<<< HEAD
-    mongo_uri: "mongodb+srv://caoan632002:0903413280An@cluster0.7wk1tu3.mongodb.net/sneaker-store"
-  }
-}
-=======
     mongo_uri:
       "mongodb+srv://caoan632002:0903413280An@cluster0.7wk1tu3.mongodb.net/test",
   },
 };
->>>>>>> 0c670fbeab07b8b429eec6d37c015d78e9bfac05
 
 module.exports = nextConfig;
